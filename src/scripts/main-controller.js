3ba3ffc39c47f97a0c794bdea7b3861267685a0a
jjnarola123 <jj@narola.email>
2023-04-11 17:17:33 +0530

    get host name using node js

--- src/scripts/main-controller.js
+++ src/scripts/main-controller.js
@@ -1,12 +1,14 @@
 const { ipcRenderer } = require('electron');
 const axios = require('axios');
 const querystring = require('querystring');
+const os = require('os') 
 
 app.controller('MainController', function ($scope, $location, myService, Constants) {
     var vm = this;
     
     vm.onMainInit = function (){
-        debugger;
+        console.log(os.hostname());
+
         let query = querystring.parse(global.location.search);
         let data = JSON.parse(query['?data']);
 
